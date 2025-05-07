5e6e1e80e59280308a4b77c538c44e2739a28aac
Princess Habon <princessmaylene53@gmail.com>
2025-05-07 11:26:12 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -30,7 +30,7 @@ pl = st.number_input("Petal Length", min_value=0.0, step=0.1)
 pw = st.number_input("Petal Width", min_value=0.0, step=0.1)
 
 
-if st.button("Clasiffy"):
+if st.button("Classify"):
     data = [[float(sl), float(sw), float(pl), float(pw)]]
     result = model.predict(data)
     result = int(round(result[0]))
